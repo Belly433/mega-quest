73ecfd32cf8f8c3bd68dbb77c5123caa0e11a4cd
Belly433 <dynellasabushimike@gmail.com>
2026-05-21 18:03:03 +0300

    Finish Phase 2 quiz dashboard and create quiz

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -38,3 +38,16 @@ export const quizApi = {
     return response.json();
   },
 };
+export const questionApi = {
+  async create(question: any) {
+    const response = await fetch("http://127.0.0.1:8000/question/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(question),
+    });
+
+    return response.json();
+  },
+};
